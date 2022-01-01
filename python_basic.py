5c707c00bf0c044d12a0be827d6f0f57cc08a0bc
yungu0010 <dbstj16@sookmyung.ac.kr>
2022-01-01 23:53:19 +0900

    220101

---
+++ python_basic.py
@@ -0,0 +1,38 @@
+#1
+print('Hello World')
+
+#2
+print("Mary's cosmetics")
+
+#3
+print("신씨가 소리질렀다. \"도둑이야\".")
+print('신씨가 소리질렀다. \"도둑이야\".')
+
+#4
+print("\"C:\\Windows\"")
+
+#5
+print("안녕하세요.\n만나서\t\t반갑습니다.")
+#\n : 줄바꿈
+#\t : 탭
+
+#6
+print("오늘은", "일요일")
+#오늘은 일요일
+#중간 공백 생김
+
+#7
+print("naver;kakao;sk;samsung")
+print("naver", "kakao", "sk", "samsung", sep=";")
+# print 함수의 sep 인자, 출력값들 사이에 한 칸의 공백 대신 들어갈 문자 지젖ㅇ
+
+#8
+print("naver/kakao/sk/samsung")
+print("naver","kakao","sk","samsung", sep="/")
+
+#9
+print("first", end=""); print("second")
+#end : 출력을 완료한 뒤 내용 수정 가능
+
+#10
+print(5/3)
