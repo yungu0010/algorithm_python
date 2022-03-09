61120705dbbcc1d89179101bcfe58b36f5abcc95
yungu0010 <dbstj16@sookmyung.ac.kr>
2022-03-10 00:40:51 +0900

    [BOJ] 10162_전자레인지

--- BOJ/Greedy/10162.py
+++ BOJ/Greedy/10162.py
@@ -1,14 +1,15 @@
 # A - 5분, B - 1분, C - 10초
+# 300s, 60s, 10s 로 큰 단위가 작은 단위의 배수
 
 time = int(input())
 
-cook_t = [0, 0, 0]
-array = [300, 60, 10]
+cook_t = [0, 0, 0]          # 각각 A, B, C 의미
+array = [300, 60, 10]       #전자레인지 시간
 
 for i in range(0, 3) :
-    cook_t[i] = time // array[i]
+    cook_t[i] = time // array[i]       
     time -= array[i] * cook_t[i]
-if time > 0 :
+if time > 0 :               # A, B, C 이후 시간이 남는다면
     print(-1)
 else :
     print(cook_t[0], cook_t[1], cook_t[2])
