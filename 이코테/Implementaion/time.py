26fbfca94012a383c71d849855e156e50c2e0f67
yungu0010 <dbstj16@sookmyung.ac.kr>
2022-03-13 19:44:27 +0900

    [이코테] 시각

---
+++ 이코테/Implementaion/time.py
@@ -0,0 +1,14 @@
+# 시각 - 완전 탐색
+# 정수 N이 입력되면 00시 00분 00초부터 N시 59분 59초까지의 모든 시각 중에서 3이 하나라도 포함되는 경우의 수 구하기
+# 시간 제한 2초
+
+n = int(input())
+count = 0
+
+for h in range(0, n+1) :
+    for m in range(0, 60) :
+        for s in range(0, 60) :
+            sentence = str(h) + str(m) + str(s)
+            if '3' in sentence :
+                count += 1
+print(count)
