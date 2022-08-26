5f7ceaa7e623cbc866f69c9b64a3d6e7d20a7660
yungu0010 <dbstj16@sookmyung.ac.kr>
2022-08-26 22:53:15 +0900

    [BOJ] 2275_부녀회장이 될테야(메모이제이션, 재귀)

--- BOJ/Mathematics/2275.py
+++ BOJ/Mathematics/2275.py
@@ -1,26 +1,39 @@
 # 2275 - 부녀회장이 될테야
 
-from curses import REPORT_MOUSE_POSITION
 import sys
 input = sys.stdin.readline
 
-def resident(k, n):
-    global human
-    floor = k
-    room = n
-
-    if floor == 0 or room == 1:
-        human += room
-        return human
-
-    resident(floor, room - 1) + resident(floor - 1, room)
-    return human
-
 testCase = int(input().rstrip())
-human = 0
+human = []
 
 for _ in range(testCase):
-    human = 0
-    k = int(input().rstrip())
-    n = int(input().rstrip())
-    print(resident(k, n))
+    floor = int(input().rstrip())           # 층 = k
+    num = int(input().rstrip())           # 호수 = n
+    arr = [x+1 for x in range(0, num)]
+
+    for i in range(floor):                  # 층 수만큼 반복
+        for j in range(1, num):             # num을 인덱스로 사용 num - 1호까지
+            arr[j] += arr[j-1]              # 각 층의 호수의 사람수를 업데이트
+    print(arr[-1])                          # 가장 마지막층 마지막 호수의 사람 수 출력
+
+
+# def resident(k, n):
+#     global human
+#     floor = k
+#     room = n
+
+#     if floor == 0 or room == 1:
+#         human += room
+#         return human
+
+#     resident(floor, room - 1) + resident(floor - 1, room)
+#     return human
+
+# testCase = int(input().rstrip())
+# human = 0
+
+# for _ in range(testCase):
+#     human = 0
+#     k = int(input().rstrip())
+#     n = int(input().rstrip())
+#     print(resident(k, n))
