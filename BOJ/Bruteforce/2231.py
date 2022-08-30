cf997d62b5992dd7750231e21b5388c62048d417
yungu0010 <dbstj16@sookmyung.ac.kr>
2022-08-31 00:59:29 +0900

    [BOJ] 2231_분해합

--- BOJ/Bruteforce/2231.py
+++ BOJ/Bruteforce/2231.py
@@ -1,13 +1,14 @@
 # 2231 - 분해합
 
-N = int(input())
-result = 0
+import sys
+input = sys.stdin.readline
+
+N = int(input().rstrip())
+result = 0              # 생성자가 없을 때는 0을 출력
 
 for i in range(1, N + 1):
-  tmp = i + sum(map(int,str(i)))
-
-  if tmp == N:
-    result = i
-    break
-
+    temp = i + sum(map(int, str(i)))
+    if temp == N:
+        result = i
+        break
 print(result)
