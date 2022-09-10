9703794363a6b0f912bfb65c4c3379766fca447b
yungu0010 <dbstj16@sookmyung.ac.kr>
2022-09-11 03:37:03 +0900

    [Del] 폴더 정리

--- BOJ/Data Structures/12605.py
+++
@@ -1,8 +0,0 @@
-# 12605 - 단어순서 뒤집기
-
-from sys import stdin
-input = stdin.readline
-n = int(input().rstrip())           # 입력받을 테스트 케이스 수 
-for i in range(n) :
-    string = list(input().split())
-    print("Case #{}: {}".format(i+1, ' '.join(string[::-1])))       # 공백을 기준으로 역순으로 문자열 join
