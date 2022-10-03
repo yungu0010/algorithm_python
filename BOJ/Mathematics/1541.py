1bfaa2ccf7e79b159fd3c27a3e4765a32b0f697e
yungu0010 <dbstj16@sookmyung.ac.kr>
2022-10-04 04:28:00 +0900

    [BOJ] 1541_잃어버린 괄호(주석 추가)

--- BOJ/Mathematics/1541.py
+++ BOJ/Mathematics/1541.py
@@ -8,10 +8,10 @@ result = 0
 
 for i in range(len(exp)):
     plus = 0
-    plusString = list(map(int, exp[i].split('+')))
+    plusString = list(map(int, exp[i].split('+')))      # -를 기준으로 나눈 수식 계산
     for j in plusString:
         plus += j
-    if i == 0:
+    if i == 0:                                          # 첫 번째 수는 -가 오지 않기 때문
         result = plus
     else:
         result -= plus
